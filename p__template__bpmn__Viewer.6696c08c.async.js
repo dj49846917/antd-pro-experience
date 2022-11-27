365dfab674b6a9f911493c4fd412f45420c548ca
dj49846917 <821084785@qq.com>
2022-11-27 20:15:22 +0800

    Updates

---
+++ p__template__bpmn__Viewer.6696c08c.async.js
@@ -0,0 +1,144 @@
+(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[7253],{49301:function(s,i,n){"use strict";n.r(i);var a=n(39428),t=n(3182),B=n(67294),m=n(23892),E=n(47087),l=n(75639),g=n(85893);function C(b){(0,B.useLayoutEffect)(function(){c()},[]);var c=function(){var p=(0,t.Z)((0,a.Z)().mark(function o(){var e;return(0,a.Z)().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return e=new E.Z({container:"#canvas",additionalModules:[m.Z],overlays:{defaults:{show:{minZoom:1},scale:!0}}}),d.next=3,e.importXML(l.Z);case 3:e.get("canvas").zoom("fit-viewport"),e.get("canvas").addMarker("START_PROCESS","highlightNow"),e.get("canvas").addMarker("sid-E49425CF-8287-4798-B622-D2A7D78EF00B","highlight");case 6:case"end":return d.stop()}},o)}));return function(){return p.apply(this,arguments)}}();return(0,g.jsx)("div",{id:"canvas",className:"bpmn-viewer"})}i.default=C},75639:function(s,i){"use strict";i.Z=`
+<?xml version="1.0" encoding="UTF-8"?>
+<definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL"
+             xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+             xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC"
+             xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI"
+             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+             expressionLanguage="http://www.w3.org/1999/XPath"
+             typeLanguage="http://www.w3.org/2001/XMLSchema"
+             targetNamespace=""
+             xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL http://www.omg.org/spec/BPMN/2.0/20100501/BPMN20.xsd">
+<collaboration id="sid-c0e745ff-361e-4afb-8c8d-2a1fc32b1424">
+    <participant id="sid-87F4C1D6-25E1-4A45-9DA7-AD945993D06F" name="Customer" processRef="sid-C3803939-0872-457F-8336-EAE484DC4A04">
+    </participant>
+</collaboration>
+<process id="sid-C3803939-0872-457F-8336-EAE484DC4A04" isClosed="false" isExecutable="false" name="Customer" processType="None">
+    <extensionElements/>
+    <laneSet id="sid-b167d0d7-e761-4636-9200-76b7f0e8e83a">
+        <lane id="sid-57E4FE0D-18E4-478D-BC5D-B15164E93254">
+            <flowNodeRef>START_PROCESS</flowNodeRef>
+            <flowNodeRef>SCAN_QR_CODE</flowNodeRef>
+            <flowNodeRef>SCAN_OK</flowNodeRef>
+            <flowNodeRef>sid-E49425CF-8287-4798-B622-D2A7D78EF00B</flowNodeRef>
+            <flowNodeRef>END_PROCESS</flowNodeRef>
+            <flowNodeRef>sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9</flowNodeRef>
+        </lane>
+    </laneSet>
+    <startEvent id="START_PROCESS" name="Notices&#10;QR code">
+        <outgoing>sid-7B791A11-2F2E-4D80-AFB3-91A02CF2B4FD</outgoing>
+    </startEvent>
+    <task completionQuantity="1" id="SCAN_QR_CODE" isForCompensation="false" name="Scan QR code" startQuantity="1">
+        <incoming>sid-4DC479E5-5C20-4948-BCFC-9EC5E2F66D8D</incoming>
+        <outgoing>sid-EE8A7BA0-5D66-4F8B-80E3-CC2751B3856A</outgoing>
+    </task>
+    <exclusiveGateway gatewayDirection="Diverging" id="SCAN_OK" name="Scan successful?&#10;">
+        <incoming>sid-EE8A7BA0-5D66-4F8B-80E3-CC2751B3856A</incoming>
+        <outgoing>sid-8B820AF5-DC5C-4618-B854-E08B71FB55CB</outgoing>
+        <outgoing>sid-337A23B9-A923-4CCE-B613-3E247B773CCE</outgoing>
+    </exclusiveGateway>
+    <task completionQuantity="1" id="sid-E49425CF-8287-4798-B622-D2A7D78EF00B" isForCompensation="false" name="Open product information in mobile  app" startQuantity="1">
+        <incoming>sid-8B820AF5-DC5C-4618-B854-E08B71FB55CB</incoming>
+        <outgoing>sid-57EB1F24-BD94-479A-BF1F-57F1EAA19C6C</outgoing>
+    </task>
+    <endEvent id="END_PROCESS" name="Is informed">
+        <incoming>sid-57EB1F24-BD94-479A-BF1F-57F1EAA19C6C</incoming>
+    </endEvent>
+    <exclusiveGateway gatewayDirection="Converging" id="sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9">
+        <incoming>sid-7B791A11-2F2E-4D80-AFB3-91A02CF2B4FD</incoming>
+        <incoming>sid-337A23B9-A923-4CCE-B613-3E247B773CCE</incoming>
+        <outgoing>sid-4DC479E5-5C20-4948-BCFC-9EC5E2F66D8D</outgoing>
+    </exclusiveGateway>
+    <sequenceFlow id="sid-7B791A11-2F2E-4D80-AFB3-91A02CF2B4FD" sourceRef="START_PROCESS" targetRef="sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9"/>
+    <sequenceFlow id="sid-EE8A7BA0-5D66-4F8B-80E3-CC2751B3856A" sourceRef="SCAN_QR_CODE" targetRef="SCAN_OK"/>
+    <sequenceFlow id="sid-57EB1F24-BD94-479A-BF1F-57F1EAA19C6C" sourceRef="sid-E49425CF-8287-4798-B622-D2A7D78EF00B" targetRef="END_PROCESS"/>
+    <sequenceFlow id="sid-8B820AF5-DC5C-4618-B854-E08B71FB55CB" name="No" sourceRef="SCAN_OK" targetRef="sid-E49425CF-8287-4798-B622-D2A7D78EF00B"/>
+    <sequenceFlow id="sid-4DC479E5-5C20-4948-BCFC-9EC5E2F66D8D" sourceRef="sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9" targetRef="SCAN_QR_CODE"/>
+    <sequenceFlow id="sid-337A23B9-A923-4CCE-B613-3E247B773CCE" name="Yes" sourceRef="SCAN_OK" targetRef="sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9"/>
+</process>
+<bpmndi:BPMNDiagram id="sid-74620812-92c4-44e5-949c-aa47393d3830">
+    <bpmndi:BPMNPlane bpmnElement="sid-c0e745ff-361e-4afb-8c8d-2a1fc32b1424" id="sid-cdcae759-2af7-4a6d-bd02-53f3352a731d">
+        <bpmndi:BPMNShape bpmnElement="sid-87F4C1D6-25E1-4A45-9DA7-AD945993D06F" id="sid-87F4C1D6-25E1-4A45-9DA7-AD945993D06F_gui" isHorizontal="true">
+            <omgdc:Bounds height="250.0" width="933.0" x="42.5" y="75.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-84cb49fd-2f7c-44fb-8950-83c3fa153d3b">
+                <omgdc:Bounds height="59.142852783203125" width="12.000000000000014" x="47.49999999999999" y="170.42857360839844"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="sid-57E4FE0D-18E4-478D-BC5D-B15164E93254" id="sid-57E4FE0D-18E4-478D-BC5D-B15164E93254_gui" isHorizontal="true">
+            <omgdc:Bounds height="250.0" width="903.0" x="72.5" y="75.0"/>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="START_PROCESS" id="START_PROCESS_gui">
+            <omgdc:Bounds height="30.0" width="30.0" x="150.0" y="165.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-e0502d32-f8d1-41cf-9c4a-cbb49fecf581">
+                <omgdc:Bounds height="22.0" width="46.35714340209961" x="141.8214282989502" y="197.0"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="SCAN_QR_CODE" id="SCAN_QR_CODE_gui">
+            <omgdc:Bounds height="80.0" width="100.0" x="352.5" y="140.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-84cb49fd-2f7c-44fb-8950-83c3fa153d3b">
+                <omgdc:Bounds height="12.0" width="84.0" x="360.5" y="172.0"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="SCAN_OK" id="SCAN_OK_gui" isMarkerVisible="true">
+            <omgdc:Bounds height="40.0" width="40.0" x="550.0" y="160.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-e0502d32-f8d1-41cf-9c4a-cbb49fecf581">
+                <omgdc:Bounds height="12.0" width="102.0" x="521.0" y="127.0"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="sid-E49425CF-8287-4798-B622-D2A7D78EF00B" id="sid-E49425CF-8287-4798-B622-D2A7D78EF00B_gui">
+            <omgdc:Bounds height="80.0" width="100.0" x="687.5" y="140.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-84cb49fd-2f7c-44fb-8950-83c3fa153d3b">
+                <omgdc:Bounds height="36.0" width="83.14285278320312" x="695.9285736083984" y="162.0"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="END_PROCESS" id="END_PROCESS_gui">
+            <omgdc:Bounds height="28.0" width="28.0" x="865.0" y="166.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-e0502d32-f8d1-41cf-9c4a-cbb49fecf581">
+                <omgdc:Bounds height="11.0" width="62.857147216796875" x="847.5714263916016" y="196.0"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNShape bpmnElement="sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9" id="sid-5134932A-1863-4FFA-BB3C-A4B4078B11A9_gui" isMarkerVisible="true">
+            <omgdc:Bounds height="40.0" width="40.0" x="240.0" y="160.0"/>
+        </bpmndi:BPMNShape>
+        <bpmndi:BPMNEdge bpmnElement="sid-EE8A7BA0-5D66-4F8B-80E3-CC2751B3856A" id="sid-EE8A7BA0-5D66-4F8B-80E3-CC2751B3856A_gui">
+            <omgdi:waypoint x="452.5" y="180"/>
+            <omgdi:waypoint x="550.0" y="180"/>
+        </bpmndi:BPMNEdge>
+        <bpmndi:BPMNEdge bpmnElement="sid-8B820AF5-DC5C-4618-B854-E08B71FB55CB" id="sid-8B820AF5-DC5C-4618-B854-E08B71FB55CB_gui">
+            <omgdi:waypoint x="590.0" y="180"/>
+            <omgdi:waypoint x="687.5" y="180"/>
+            <bpmndi:BPMNLabel labelStyle="sid-e0502d32-f8d1-41cf-9c4a-cbb49fecf581">
+                <omgdc:Bounds height="12.048704338048935" width="16.32155963195521" x="597.8850936986571" y="155"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNEdge>
+        <bpmndi:BPMNEdge bpmnElement="sid-7B791A11-2F2E-4D80-AFB3-91A02CF2B4FD" id="sid-7B791A11-2F2E-4D80-AFB3-91A02CF2B4FD_gui">
+            <omgdi:waypoint x="180.0" y="180"/>
+            <omgdi:waypoint x="240.0" y="180"/>
+        </bpmndi:BPMNEdge>
+        <bpmndi:BPMNEdge bpmnElement="sid-4DC479E5-5C20-4948-BCFC-9EC5E2F66D8D" id="sid-4DC479E5-5C20-4948-BCFC-9EC5E2F66D8D_gui">
+            <omgdi:waypoint x="280.0" y="180"/>
+            <omgdi:waypoint x="352.5" y="180"/>
+        </bpmndi:BPMNEdge>
+        <bpmndi:BPMNEdge bpmnElement="sid-57EB1F24-BD94-479A-BF1F-57F1EAA19C6C" id="sid-57EB1F24-BD94-479A-BF1F-57F1EAA19C6C_gui">
+            <omgdi:waypoint x="787.5" y="180.0"/>
+            <omgdi:waypoint x="865.0" y="180.0"/>
+        </bpmndi:BPMNEdge>
+        <bpmndi:BPMNEdge bpmnElement="sid-337A23B9-A923-4CCE-B613-3E247B773CCE" id="sid-337A23B9-A923-4CCE-B613-3E247B773CCE_gui">
+            <omgdi:waypoint x="570.5" y="200.0"/>
+            <omgdi:waypoint x="570.5" y="269.0"/>
+            <omgdi:waypoint x="260.5" y="269.0"/>
+            <omgdi:waypoint x="260.5" y="200.0"/>
+            <bpmndi:BPMNLabel labelStyle="sid-e0502d32-f8d1-41cf-9c4a-cbb49fecf581">
+                <omgdc:Bounds height="21.4285888671875" width="12.0" x="550" y="205"/>
+            </bpmndi:BPMNLabel>
+        </bpmndi:BPMNEdge>
+    </bpmndi:BPMNPlane>
+    <bpmndi:BPMNLabelStyle id="sid-e0502d32-f8d1-41cf-9c4a-cbb49fecf581">
+        <omgdc:Font isBold="false" isItalic="false" isStrikeThrough="false" isUnderline="false" name="Arial" size="11.0"/>
+    </bpmndi:BPMNLabelStyle>
+    <bpmndi:BPMNLabelStyle id="sid-84cb49fd-2f7c-44fb-8950-83c3fa153d3b">
+        <omgdc:Font isBold="false" isItalic="false" isStrikeThrough="false" isUnderline="false" name="Arial" size="12.0"/>
+    </bpmndi:BPMNLabelStyle>
+</bpmndi:BPMNDiagram>
+</definitions>
+`}}]);
